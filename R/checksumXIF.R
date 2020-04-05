54787f4bdff4e8d34f724d5d1b9f7ffd361402e6
unknown <git.demont@gmail.com>
2020-04-05 17:25:13 +0200

    add extra param

--- R/checksumXIF.R
+++ R/checksumXIF.R
@@ -3,6 +3,7 @@
 #' This function returns RIF/CIF checksum.
 #' Checksum is the sum of img IFDs (Image Field Directory) offsets of objects 0, 1, 2, 3 and 4.
 #' @param fileName path to file.
+#' @param ... other arguments to be passed.
 #' @keywords internal
 checksumXIF = function(fileName, ...) {
   # TODO ask AMNIS how checksum is computed
