b8ef38247710157a127e763c88b741391c679661
gitdemont <git.demont@gmail.com>
2020-06-04 09:43:38 +0200

    add header

--- R/ExportToGallery.R
+++ R/ExportToGallery.R
@@ -1,3 +1,30 @@
+################################################################################
+# This file is released under the GNU General Public License, Version 3, GPL-3 #
+#                                                                              #
+# It is part of IFC package, please cite:                                      #
+# -IFC: An R Package for Imaging Flow Cytometry                                #
+# -YEAR: 2020                                                                  #
+# -COPYRIGHT HOLDERS: Yohann Demont, INSERM, UPD, CHU Amiens                   #
+# -Copyright (C) 2020 Yohann Demont                                            #
+#                                                                              #
+# DISCLAIMER:                                                                  #
+# -You are using this package on your own risk!                                #
+# -We do not guarantee privacy nor confidentiality.                            #
+# -This program is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE. In no event shall the copyright holders or #
+# contributors be liable for any direct, indirect, incidental, special,        #
+# exemplary, or consequential damages (including, but not limited to,          #
+# procurement of substitute goods or services; loss of use, data, or profits;  #
+# or business interruption) however caused and on any theory of liability,     #
+# whether in contract, strict liability, or tort (including negligence or      #
+# otherwise) arising in any way out of the use of this software, even if       #
+# advised of the possibility of such damage.                                   #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with IFC. If not, see <http://www.gnu.org/licenses/>.                  #
+################################################################################
+
 #' @title Gallery Export
 #' @description
 #' Exports gallery of `IFC_img` / `IFC_msk`objects
