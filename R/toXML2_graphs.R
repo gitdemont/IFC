e1d7394fc174cdfa919b33daf7c26a5ae545f461
unknown <git.demont@gmail.com>
2021-09-08 16:14:07 +0200

    modify toXML2_graphs because Display node can contain other elements than graphs (e.g. statistics, images)

--- R/toXML2_graphs.R
+++ R/toXML2_graphs.R
@@ -52,12 +52,11 @@ toXML2_graphs = function(graphs, verbose = FALSE) {
     g = g[sapply(g, length) != 0] # to remove empty values (e.g. xtrans, ytrans)
     g
   })
-  xml_new_node(name = "Displays", attrs = list(count = num_to_string(length(graphs)), layout="1", entriesPerRow="12", lightDarkMode="1"),
-             .children = lapply(graphs, FUN=function(i_graph) {
+  lapply(graphs, FUN=function(i_graph) {
                xml_new_node(name = "Graph", attrs = i_graph[!grepl("Legend|BasePop|GraphRegion|ShownPop", names(i_graph))],
                           .children = c(lapply(i_graph[["Legend"]], FUN=function(i) xml_new_node(name = "Legend", attrs = i)),
                                         lapply(i_graph[["BasePop"]], FUN=function(i) xml_new_node(name = "BasePop", attrs = i)),
                                         lapply(i_graph[["GraphRegion"]], FUN=function(i) xml_new_node(name = "GraphRegion", attrs = i)),
                                         lapply(i_graph[["ShownPop"]], FUN=function(i) xml_new_node(name = "ShownPop", attrs = i))))
-  }))
+  })
 }
